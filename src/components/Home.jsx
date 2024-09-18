f778b48e9e870ab279fb7bb660ab766e037acf29
unknown <saxenahrithik2000@gmail.com>
2024-09-19 00:20:25 +0530

    added useNavigate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+    const navigate = useNavigate();
     return ( 
         <>
             <h1>Home page</h1>
+            <div>
+                <button onClick={()=>navigate("/login")}>Go to Login Page</button>
+                <button onClick={()=>navigate("/users")}>Go to Users Page</button>
+            </div>
         </>
      );
 }
